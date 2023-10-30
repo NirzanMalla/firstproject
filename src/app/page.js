021767178841dd07786e4c2da5ced52967cadf53
Nirajan <nirzanmalla@gmail.com>
2023-10-30 13:10:41 +0545

    This is the first commit

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
         </p>
+        <div>Hy I am Nirajan Bikram Malla</div>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
